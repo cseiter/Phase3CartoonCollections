6fa4ef1dddaa8e44e978e56bb14c4cbdd56572c8
chris <cseiter@gmail.com>
2021-07-05 19:19:42 -0400

    all tests passed

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,14 +7,13 @@ def summon_captain_planet(calls_array)
 end
 
 def long_planeteer_calls(long_calls_array)
-  # code an argument here
-  # Your code here
   long_calls_array.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_exists)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_exists.find {|food| cheese_types.include?(food)}
 end
 
 roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"] )
