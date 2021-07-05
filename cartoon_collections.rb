67fa21a6322c81454c2d5a097e60e6078f9ddf67
chris <cseiter@gmail.com>
2021-07-05 19:13:57 -0400

    long_planetter_calls method pass test

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,15 @@
 def roll_call_dwarves(dwarf_array)
-  # code an argument here
-  # Your code here
   dwarf_array.each_with_index {|dwarf, index| puts "#{index+1}. #{dwarf}"}
 end
 
 def summon_captain_planet(calls_array)
-  # code an argument here
-  # Your code here
   calls_array.map {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(long_calls_array)
+  # code an argument here
   # Your code here
+  long_calls_array.any? {|call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
