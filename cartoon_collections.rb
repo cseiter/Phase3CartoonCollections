3ada02fa920f431c7b4670a2b52908fabf26bf61
chris <cseiter@gmail.com>
2021-07-05 19:10:31 -0400

    summon_captain_planet method pass test

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,10 @@ def roll_call_dwarves(dwarf_array)
   dwarf_array.each_with_index {|dwarf, index| puts "#{index+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls_array)
+  # code an argument here
   # Your code here
+  calls_array.map {|call| call.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
