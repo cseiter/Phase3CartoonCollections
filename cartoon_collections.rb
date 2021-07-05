f2be08eb3f8da8286e680b8f43800fdec1596241
chris <cseiter@gmail.com>
2021-07-05 19:20:00 -0400

    cleanup

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,10 +10,7 @@ def long_planeteer_calls(long_calls_array)
   long_calls_array.any? {|call| call.length > 4}
 end
 
-def find_the_cheese(cheese_exists)# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese_exists)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_exists.find {|food| cheese_types.include?(food)}
 end
-
-roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"] )
