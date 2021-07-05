7e6a3e8abd13bb050b69c9474135ab82ec07ba6f
chris <cseiter@gmail.com>
2021-07-05 19:06:46 -0400

    roll_call_dwarves component passed test

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarf_array)
+  # code an argument here
   # Your code here
+  dwarf_array.each_with_index {|dwarf, index| puts "#{index+1}. #{dwarf}"}
 end
 
 def summon_captain_planet# code an argument here
@@ -14,3 +16,5 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"] )
